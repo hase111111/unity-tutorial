9b3727bede4d34768c32abb4a9a66d78993bbab7
hase111111 <hasehasehase61@gmail.com>
2024-11-01 23:38:49 +0900

    プレイヤーの動作の完成

--- Pazzlock/Assets/PlayerMover.cs
+++ Pazzlock/Assets/PlayerMover.cs
@@ -6,6 +6,8 @@ public class PlayerMover : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] float _speed = 5.0f;
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _spriteRenderer;
 
     float _velocityX = 0f;
     float _velocityY = 0f;
@@ -17,5 +19,23 @@ public class PlayerMover : MonoBehaviour
         _velocityY = Input.GetAxis("Vertical") * _speed;
 
         _rb.velocity = new Vector2(_velocityX, _velocityY);
+
+        if (_velocityX != 0 || _velocityY != 0)
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else 
+        {
+            _animator.SetBool("isWalking", false);
+        }
+
+        if (_velocityX > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_velocityX < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 }
